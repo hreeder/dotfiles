91e6f9cf47ead93ade77ed9dc27098f453ef567b
Harry Reeder <harry@harryreeder.co.uk>
2021-03-26 19:43:52 +0000

    Update AWS configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 if [[ -v CODESPACES ]]; then
   sudo apt-get update
-  sudo apt-get install -y awscli
-  sudo pip3 install aws-mfa j2cli
+  sudo apt-get install -y python3-pip
+  sudo pip3 install --upgrade awscli aws-mfa j2cli
 
   mkdir ~/.aws
   j2 -o ~/.aws/config templates/aws_config.j2
