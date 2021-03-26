bb2f66caafbd7df6aa2c745758c796a14a499c37
Harry Reeder <harry@harryreeder.co.uk>
2021-03-26 19:10:23 +0000

    feat: Add AWS templating

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,8 @@ if [[ -v CODESPACES ]]; then
   sudo apt-get update
   sudo apt-get install awscli
   sudo pip3 install aws-mfa j2cli
+
+  mkdir ~/.aws
+  j2 -o ~/.aws/config templates/aws_config.j2
+  j2 -o ~/.aws/credentials templates/aws_credentials.j2
 fi
